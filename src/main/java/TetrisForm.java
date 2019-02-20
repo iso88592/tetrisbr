e0a50ba9d5aa99542508a20200d66f10b7f3274b
csiso <kristofkubinyi@gmail.com>
2019-02-20 13:21:54 +0100

    Basic form of the client game board.

--- src/main/java/TetrisForm.java
+++ src/main/java/TetrisForm.java
@@ -2,6 +2,20 @@ import javax.swing.*;
 
 public class TetrisForm extends JFrame {
     private JPanel tetrisPanel;
+    private JPanel Panel_Enemy_1;
+    private JPanel Panel_Enemy_2;
+    private JPanel Panel_Enemy_3;
+    private JPanel Panel_Enemy_4;
+    private JPanel Panel_Main;
+    private JLabel Label_Enemy_4;
+    private JLabel Label_Enemy_3;
+    private JLabel Label_Enemy_2;
+    private JLabel Label_Enemy_1;
+    private JPanel Panel_NextItem;
+    private JLabel Label_NextItem;
+    private JLabel Label_YP;
+    private JLabel Label_Numb_YP;
+    private JLabel Label_Cím;
 
     public TetrisForm() {
         super("Tetris battle royale");
@@ -11,5 +25,9 @@ public class TetrisForm extends JFrame {
         pack();
         this.setSize(800, 800);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        TetrisForm ujkliens = new TetrisForm();
     }
 }
