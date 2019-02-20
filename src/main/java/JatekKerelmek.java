e37b995cadde5071390c36400b5626eb4738b084
Csonka <csonka.viktor@gmail.com>
2019-02-20 13:03:03 +0100

    Form can be started

--- src/main/java/JatekKerelmek.java
+++ src/main/java/JatekKerelmek.java
@@ -1,3 +1,17 @@
+import java.util.Vector;
+
 public class JatekKerelmek {
 
+    public Vector<Player> players = new Vector<Player>();
+
+    public String joined(Player player){
+        if(player.active){
+            players.add(player);
+            return "Joined";
+        }
+        else
+        {
+            return "";
+        }
+    }
 }
--- src/main/java/Lobby.java
+++ src/main/java/Lobby.java
@@ -2,29 +2,8 @@ import java.io.IOException;
 import java.util.Vector;
 
 public class Lobby {
-
-
-    Vector<Player> players = new Vector<Player>();
-
-
-    public String joined(Player player){
-        if(player.active){
-            players.add(player);
-            return "Joined";
-        }
-        else
-        {
-            return "";
-        }
-    }
     public void start() throws IOException {
-        if(players.size() == 5)
-        {
-            for (Player player: players) {
-                String response = "Amit szeretnél hogy milyen szöveget írjon ki";
-                player.out.writeObject(response);
-            }
-        }
+
     }
 
 
