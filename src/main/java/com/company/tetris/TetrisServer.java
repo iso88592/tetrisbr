ae9e010bd6e76dc2311759bacdad8983ccdb67c7
Eszti <eszter.otruba@gmail.com>
2019-02-26 11:50:38 +0100

    html hozzáadva

--- src/main/java/com/company/tetris/TetrisServer.java
+++ src/main/java/com/company/tetris/TetrisServer.java
@@ -4,6 +4,9 @@ import Logic.Round;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 @RestController
 public class TetrisServer {
 
@@ -12,5 +15,26 @@ public class TetrisServer {
     @RequestMapping(value = "/")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping
+    public String tetris(){
+        String url=getClass().getClassLoader().getResource("html/tetris.html").getFile();
+        StringBuilder result=new StringBuilder();
+
+        try {
+            BufferedReader br= new BufferedReader(new FileReader(url));
+            String sor;
+            while ((sor=br.readLine())!=null){
+                result.append(sor);
+
+            }
+            return result.toString();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return "ajjaj";
+        }
+
     }
 }
