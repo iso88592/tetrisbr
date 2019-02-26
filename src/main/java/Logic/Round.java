287359408862cd33d344997ab42328fd735f7908
Csonka <csonka.viktor@gmail.com>
2019-02-26 10:51:03 +0100

    Round and TetrisField created bases of game are added field log added for testing

---
+++ src/main/java/Logic/Round.java
@@ -0,0 +1,15 @@
+package Logic;
+
+public class Round {
+
+    private TetrisField tetrisField;
+
+    public Round () {
+        tetrisField = new TetrisField();
+        this.Start();
+    }
+
+    private void Start() {
+
+    }
+}
