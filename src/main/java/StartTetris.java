489e7c94fd54d11ef88f0b3832b39b3fdd8d174d
Norbi <kovinor123@gmail.com>
2019-02-26 11:16:04 +0100

    szerver új kört hoz létre

--- src/main/java/StartTetris.java
+++
@@ -1,5 +0,0 @@
-public class StartTetris {
-    public static void main(String[] args) {
-        TetrisForm tetrisForm = new TetrisForm();
-    }
-}
--- src/main/java/TetrisForm.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class TetrisForm {
-    private JPanel tetrisPanel;
-
-    public TetrisForm() {
-        System.out.println("hát ez a gergő 69");
-    }
-}
