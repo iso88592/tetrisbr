70622fbb842b5753784df648bee75d89ceafde95
David White <neumannjanesz@hotmail.com>
2019-02-26 11:40:32 +0100

    Player osztály round

--- src/main/java/com/company/tetris/Player.java
+++ src/main/java/com/company/tetris/Player.java
@@ -1,4 +1,6 @@
-package Logic;
+package com.company.tetris;
+
+import Logic.Round;
 
 import javax.servlet.http.Cookie;
 
@@ -7,10 +9,12 @@ public class Player {
     String name;
     int score;
     Cookie cookie;
+    Round round;
 
-    public Player(String name, Cookie cookie){
+    public Player(String name, Cookie cookie, Round round){
         name=this.name;
         cookie=this.cookie;
+        round=this.round;
     }
 
 }
