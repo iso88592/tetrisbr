ff2fd187a9b3c934828468ace247076f75ca727f
Neumann Jonasz <neumannjanesz@hotmail.com>
2019-02-26 10:30:51 +0100

    teszt

--- src/main/java/TetrisForm.java
+++ src/main/java/TetrisForm.java
@@ -4,6 +4,7 @@ public class TetrisForm {
     private JPanel tetrisPanel;
 
     public TetrisForm() {
+        //faszommmm
         System.out.println("hát ez a gergő 3");
         System.out.println("Elég veszélyes");
     }
